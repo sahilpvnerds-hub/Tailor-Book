ffc0236a62c1678d157da6fe0fc4fc726a0cdcd1
lavyiion072 <lavp.yiion@gmail.com>
2026-07-05 16:57:54 +0530

    Fix EAS and Expo config

--- artifacts/mobile/babel.config.js
+++ artifacts/mobile/babel.config.js
@@ -3,6 +3,22 @@ module.exports = function (api) {
   return {
     presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
     plugins: [
+      function () {
+        return {
+          visitor: {
+            MemberExpression(path) {
+              if (
+                path.node.object.type === "MemberExpression" &&
+                path.node.object.object.name === "process" &&
+                path.node.object.property.name === "env" &&
+                path.node.property.name === "EXPO_ROUTER_APP_ROOT"
+              ) {
+                path.replaceWith({ type: "StringLiteral", value: "../../app" });
+              }
+            }
+          }
+        };
+      },
       "react-native-worklets/plugin", // MUST be last per react-native-reanimated docs
     ],
   };
