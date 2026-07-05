426d61c675a1bebb06655ed0e9811046133245ce
lavyiion072 <lavp.yiion@gmail.com>
2026-07-05 16:32:01 +0530

    feat: add babel.config.js with expo preset and react-native-worklets plugin configuration

--- artifacts/mobile/babel.config.js
+++ artifacts/mobile/babel.config.js
@@ -1,27 +1,8 @@
-const path = require("path");
-const appRoot = path.resolve(__dirname, "./app");
-
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
     plugins: [
-      function () {
-        return {
-          visitor: {
-            MemberExpression(path) {
-              if (
-                path.node.object.type === "MemberExpression" &&
-                path.node.object.object.name === "process" &&
-                path.node.object.property.name === "env" &&
-                path.node.property.name === "EXPO_ROUTER_APP_ROOT"
-              ) {
-                path.replaceWith({ type: "StringLiteral", value: appRoot });
-              }
-            }
-          }
-        };
-      },
       "react-native-worklets/plugin", // MUST be last per react-native-reanimated docs
     ],
   };
