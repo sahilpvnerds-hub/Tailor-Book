65a0df1578661bf547514ccad7dc5c71a248a471
lavyiion072 <lavp.yiion@gmail.com>
2026-07-21 16:44:38 +0530

    feat: initialize mobile build artifacts with custom Babel configuration and bundling setup

---
+++ artifacts/mobile/babel-preset-private-properties.js
@@ -0,0 +1,7 @@
+module.exports = () => ({
+  plugins: [
+    "@babel/plugin-transform-class-properties",
+    "@babel/plugin-transform-private-methods",
+    "@babel/plugin-transform-private-property-in-object"
+  ]
+});
--- artifacts/mobile/babel.config.js
+++ artifacts/mobile/babel.config.js
@@ -1,11 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
+    presets: [
+      ["babel-preset-expo", { unstable_transformImportMeta: true }]
+    ],
     plugins: [
-      ["@babel/plugin-transform-typescript", { isTSX: true, allExtensions: true }],
-      "@babel/plugin-transform-class-properties",
-      "@babel/plugin-transform-private-methods",
       function () {
         return {
            visitor: {
